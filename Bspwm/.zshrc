bbed2e4aca9618304888c66a62856fd73601a2bc
Tanish2002 <tanishkhare@gmail.com>
2020-03-04 16:27:21 +0530

    Big Changes

--- Bspwm/.zshrc
+++ Bspwm/.zshrc
@@ -8,6 +8,7 @@ export ZSH=$HOME/.oh-my-zsh
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 ZSH_THEME="powerlevel10k/powerlevel10k"
+#eval "$(starship init zsh)"
 
 # Autojump
 #. /usr/share/autojump/autojump.zsh
---
+++ bin/bin/song-name.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# http://github.com/mitchweaver/bin
+#
+# nicely display current song from mmt (Mitch's MPV Tool)
+#
+
+artist=$($HOME/bin/mpvctl -k artist)
+title=$($HOME/bin/mpvctl -k title)
+
+if [ "$artist" != null ] && [ "$title" != null ] ; then
+    printf '%s - %s\n' "$artist" "$title"
+else
+    res=$($HOME/bin/mpvctl -k filename)
+    case $res in
+        *'.'*)
+            # must be a local file
+            printf '%s\n' "$res"
+            ;;
+        *)
+            # youtube watch codes are always 11 chars long
+            if [ ${#res} -eq 11 ] ; then
+                pgrep -f youtube-dl >/dev/null ||
+                ytdl=$(youtube-dl -e "http://youtube.com/watch?v=$res" 2>/dev/null)
+                [ "$ytdl" ] && printf '%s\n' "$ytdl"
+            fi
+    esac
+fi
