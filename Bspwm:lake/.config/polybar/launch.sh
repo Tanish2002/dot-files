9e6853efc2a5810fe7daef58cbef02c14fcbd53a
Tanish Khare <tanishkhare@gmail.com>
2020-03-18 07:26:00 +0530

    Made some changes to polybar

--- Bspwm:lake/.config/polybar/launch.sh
+++ Bspwm:lake/.config/polybar/launch.sh
@@ -3,8 +3,9 @@
 # Terminate already running bar instances
 killall -q polybar
 
-
-
+int=$(iwgetid | getn 1)
+echo ${int}
+sed -i "s/interface = .*/interface = \"$int\"/g" ~/.config/polybar/config
 # Launch bar1 and bar2
 polybar white &
 #polybar bar2 &
--- Bspwm:tea/.config/polybar/launch.sh
+++ Bspwm:tea/.config/polybar/launch.sh
@@ -3,8 +3,8 @@
 # Terminate already running bar instances
 killall -q polybar
 
-
-
+int=$(iwgetid | getn 1)
+sed -i "s/interface = .*/interface = \"$int\"/g" ~/polybar/config
 # Launch bar1 and bar2
 polybar white &
 #polybar bar2 &
