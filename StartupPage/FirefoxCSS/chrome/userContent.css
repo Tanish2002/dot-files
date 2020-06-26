72709b2d04be71a23d59c4460844e48936c0df42
Tanish2002 <tanishkhare@gmail.com>
2020-06-26 13:53:31 +0530

    Added Dwm_solid

---
+++ StartupPage/FirefoxCSS/chrome/userContent.css
@@ -0,0 +1,25 @@
+@import url("userChrome.css");
+
+/* Removes white loading page */
+@-moz-document url(about:blank), url(about:newtab), url(about:home) {
+    html:not(#ublock0-epicker), html:not(#ublock0-epicker) body, #newtab-customize-overlay {
+      background: var(--mff-bg) !important;
+    }
+  }
+
+
+  /* Hide scrollbar */
+
+  :root{
+    scrollbar-width: thin !important;
+    scrollbar-color: #5BA7D6 #0f0f13 !important;
+  }
+
+
+  @-moz-document url(about:privatebrowsing) {
+
+  :root{
+    scrollbar-width: 1 !important;
+    scrollbar-color: #5BA7D6 !important;
+  }
+  }
